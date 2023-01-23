60912e822eb23a9d56397396af4324a958441894
Abir Sikdar <abirsikdar0729@gmail.com>
2023-01-23 22:27:23 +0530

    lesson- 03

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React!');
+// const heading = React.createElement('h1', { id: 'heading' }, 'Namaste React!');
+
+///////////////////JSX/////////////////////
+// const heading2 = <h1>Hello There</h1>;
+
+const Heading3 = () => <h2>Hello</h2>;
+
+const Heading = () => {
+  return <div>{<Heading3 />}</div>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
+root.render(<Heading />);
